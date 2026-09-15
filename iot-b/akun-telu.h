77d7f07b6fca8d47a8d4842eaac8f0af5c81adc1
Auralius Manurung <auralius@users.noreply.github.com>
2026-09-15 18:51:52 +0700

    add akun-telu.h

---
+++ iot-b/akun-telu.h
@@ -0,0 +1,8 @@
+#pragma once
+
+// -----------------------------------------------------------------------------
+// Campus Wi-Fi
+// -----------------------------------------------------------------------------
+const char* WIFI_SSID = "TelU-Connect";
+const char* WIFI_USER = "xxxxxxxxxxxxx";
+const char* WIFI_PASS = "xxxxxxxxxxxxx";
